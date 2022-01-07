40d8e176bda109b130d388b6aa17308591f362d4
acalderon1 <acalderon@themarshallproject.org>
2022-01-07 17:56:39 -0500

    trying to make the site responsive... trying

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,6 @@ if (!localStorage.getItem('name')) {
   myHeading.textContent = '¡Hola, ' + storedName + '!'
 }
 
-
 myButton.onclick = function() {
   setUserName()
 }
@@ -23,82 +22,7 @@ myButton.onclick = function() {
 
 
 
-// for (let i  = 0; i < 10; i++) {
-//   console.log(i)
-// }
 
-// array.forEach(element => {
-  
-// });
-// const x = 11
-// const color = x > 10 ? 'red' : 'blue'
-
-// switch(color) {
-//   case "red": 
-//   console.log('color is red');
-//     break;
-//   case "blue":
-//     console.log("color is blue")
-//     break;
-//   default:
-//     console.log('color is not blue')
-// }
-
-// function addNums(num1, num2) {
-//   return num1 + num2
-
-// }
-
-// console.log(addNums(5,4))
-
-// const addNums2 = (num1, num2) => {
-//   return (num1 + num2) * 10
-// }
-
-// console.log(addNums2(5,5))
-
-// constructor function
-
-// function Person(firstName, lastName, dob) {
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-//   this.dob = new Date(dob);
-
-// }
-
-// add methods to prototype of object
-
-// Person.prototype.getBirthYear = function() {
-//   this.getBirthYear = function() {
-//     return this.dob.getFullYear();
-//   }
-// }
-
-// const person1 = new Person("mary", 'smith', '3-6-1991')
-
-// console.log(person1)
-
-// class
-
-// class Person {
-//   constructor(firstName, lastName, dob) {
-  
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-//   this.dob = new Date(dob);
-
-//   }
-
-//   getBirthYear() {
-
-//   return this.dob.getFullYear()
-
-//   }
-// }
-
-// const person2 = new Person("maria", 'smitina', '6-3-1991')
-
-// console.log(person2)
 
 
 
