7399ae9a676e40f46d24dd2c6928ffaf83ea28b6
acalderon1 <acalderon@themarshallproject.org>
2022-03-25 16:17:30 -0400

    playing with fonts and weight -- not a typographer

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,27 @@
 const myButton = document.querySelector("button");
-const myHeading = document.querySelector("h2");
+const myHeading = document.querySelector("container-tagline");
 
 function setUserName() {
   const myName = prompt("Would you mind sharing your name?");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = "¡Hola, " + myName + "!";
+  if (myName === "") {
+    myHeading.textContent = "¡Hola!";
+  } else if (myName) {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "¡Hola, " + myName + "!";
+  } else {
+    myHeading.textContent = "¡Hola!";
+  }
 }
 
+// Memory
 if (!localStorage.getItem("name")) {
   setUserName();
-} else if (localStorage.getItem("name")) {
-  myHeading.textContent = "¡Hola!";
 } else {
   const storedName = localStorage.getItem("name");
   myHeading.textContent = "¡Hola, " + storedName + "!";
 }
 
+// Button functionality
 myButton.onclick = function () {
   setUserName();
 };
