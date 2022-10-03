25b1c603e17b3fd49cae1d8bf9269e6468149a66
acalderon1 <acalderon@themarshallproject.org>
2022-10-02 22:33:17 -0400

    start building svg for chart

--- src/playpen-project-2.js
+++ src/playpen-project-2.js
@@ -2,18 +2,33 @@
 import * as d3 from "d3";
 
 function drawChart() {
-  const containerID = document.getElementById("simple-viz-g");
-  console.log(containerID);
+  const containerID = document.querySelector("#simple-viz-g");
+  const aspectRatio = 16 / 12;
+  const width = containerID.clientWidth;
+  const height = width * aspectRatio;
 
-  // const userAction = async () => {
-  //     const response = await fetch('http://example.com/movies.json', {
-  //       method: 'POST',
-  //       body: myBody, // string or object
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       }
-  //     });
-  //     const myJson = await response.json(); //extract JSON from the http response
-  //     // do something with myJson
-  //   }
+  const margin = { top: 10, right: 10, left: 10, bottom: 10 };
+
+  const chartHeight = height - margin.top - margin.bottom;
+  const chartWidth = height - margin.right - margin.left;
+
+  const svg = d3.select(containerID).append("svg");
+
+  svg.attr("height", height).attr("width", width);
+
+  console.log(svg);
 }
+
+drawChart();
+
+// const userAction = async () => {
+//     const response = await fetch('http://example.com/movies.json', {
+//       method: 'POST',
+//       body: myBody, // string or object
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+//     const myJson = await response.json(); //extract JSON from the http response
+//     // do something with myJson
+//   }
